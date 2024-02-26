7f753bca21c88bcafe8035a93d41e4dcdf6df64c
alariordas <alariordas@gmail.com>
2024-02-26 13:41:55 +0100

    Proyecto Documentado

--- src/examen/RockPaperScissors.java
+++ src/examen/RockPaperScissors.java
@@ -1,15 +1,30 @@
 package examen;
 
 import java.util.*;
-
+/**
+ * Esta es la clase RockPaperScissors, esta esta compuesta por un iniciador de clases,
+ *  un main que es el que se va encargar de ejecutar la clase y un metodo que es ejecutado por el main,
+ *  el metodo recive el nombre de checkGame()
+ * @author DAM107
+ * @since 26/02/2024
+ */
 public class RockPaperScissors {
+	/**
+	 * Declaración de las variables iniciales de la clase.
+	 */
 	private static int keyboard = 0;
 	private static Scanner scanner = new Scanner(System.in);
 	public static List<String> optionList = Arrays.asList("Rock", "Paper", "Scissors");
-
+/**
+ * Iniciador del constructor por defecto
+ */
 	public RockPaperScissors() {
 	}
-
+/**
+ * Este es nuestro main el cual es encarga de ejecutar un pequeño menu que comprueba una entrada de teclado, se comrpobara que dicha entrada sea numerica y que se encuentre entre el 1 y 3
+ * a partir de eso ejecuta nuestro metodo checkGame()
+ * @param args
+ */
 	public static void main(String[] args) {
 		do {
 			System.out.println("Welcome to Rock, Paper, Scissors!");
@@ -23,7 +38,12 @@ public class RockPaperScissors {
 		System.out.println(RockPaperScissors.checkGame(computerChoice, userChoice));
 		scanner.close();
 	}
-
+/**
+ * Este es el metodo encargado de la logica del juego de piedra, papel y tijera. Comarando los 2 siguientes parametros de entrada.
+ * @param userChoice Esta variable de tipo String es la opcion que ha elegido el usuario, por ejemplo "Rock"
+ * @param computerChoice Esta variable de tipo String es la opcion que se ha generado de forma aleatoria en el main en este caso optionList.get(new Random().nextInt(3)), por ejemplo "Scissors"
+ * @return Este nos devolvera una variable de tipo String que en este ejemplo será "You win this round!"
+ */
 	public static String checkGame(String userChoice, String computerChoice) {
 		String gameResult = "";
 		
